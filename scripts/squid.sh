66af2934e2809d5f96a43d4e465edd9f94c03b89
Clément Hamada <clementhamada@protonmail.com>
2024-04-23 16:20:03 +0200

    host: Configure squid for local access and disable cache

--- scripts/squid.sh
+++ scripts/squid.sh
@@ -11,6 +11,15 @@ apt-get -yq install --no-install-recommends squid
 cat > /etc/squid/conf.d/debian.conf << EOF
 # Logs are managed by logrotate on Debian
 logfile_rotate 0
+EOF
 
+cat > /etc/squid/conf.d/localnet.conf << EOF
+# Allow connections from localnet
 http_access allow localnet
+EOF
+
+# Configure Squid to disable cache
+cat > /etc/squid/conf.d/no-cache.conf << EOF
+# Do not cache any response
+cache deny all
 EOF
