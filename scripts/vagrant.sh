92f53ae95983eeb7970780d58b4ac59bef88656e
Clément Hamada <clementhamada@protonmail.com>
2024-04-24 19:32:38 +0200

    host: Add dev hostname to /etc/hosts

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+APPS=(a b c argocd dev)
+
 export DEBIAN_FRONTEND=noninteractive
 
 # Install Vagrant
@@ -11,6 +13,7 @@ apt-get -yq install --no-install-recommends vagrant
 echo 'vagrant /vagrant vboxsf defaults 0 0' >> /etc/fstab
 
 # Add IOT hostnames to hosts
-echo '192.168.56.110	a.iot b.iot c.iot argocd.iot wil.iot' >> /etc/hosts
+echo '192.168.56.110 ${APPS[*]/%/.$domain_name}' >> /etc/hosts
 
+# Install vagrant-reload plugin
 su vagrant -c 'vagrant plugin install vagrant-reload'
